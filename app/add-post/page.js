e1a2d21dd9e858c1b2ba44d1c2f3e2a4ac6878a1
Syed Mehdi <mehdisyed3@gmail.com>
2024-01-30 13:09:04 -0500

    adding post wip

---
+++ app/add-post/page.js
@@ -0,0 +1,12 @@
+'use client'
+
+import React from 'react'
+import styles from '@/app/page.module.css'
+
+const AddPost = () => {
+  return (
+    <div className={styles.main}>Add post</div>
+  )
+}
+
+export default AddPost
--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import prisma from "./lib/prisma";
 import styles from "./page.module.css";
 import Posts from "./components/Posts";
+import Link from "next/link";
 
 async function getPosts(){
   const posts = await prisma.post.findMany({
@@ -21,6 +22,9 @@ export default async function Home() {
   console.log('>>>> post',posts)
   return (
     <main className={styles.main}>
+      <Link href={'/add-post'} >
+        Add post
+      </Link>
      
      <h1>Feed</h1>
      {
